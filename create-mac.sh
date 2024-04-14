0b52892e6fbfd6d80330c046c6128ad7ea46a804
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-04-14 13:16:26 -0500

    Add files via upload

---
+++ create-mac.sh
@@ -0,0 +1,24 @@
+hdd_size=${1:-256}
+
+sudo apt-get install qemu uml-utilities virt-manager git \
+    wget libguestfs-tools p7zip-full make dmg2img tesseract-ocr \
+    tesseract-ocr-eng genisoimage -y
+
+git clone --depth 1 --recursive https://github.com/sam-sneed/OSX-KVM-highspec/tree/master
+
+cd OSX-KVM-highspec
+
+sudo modprobe kvm; echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs
+
+sudo usermod -aG kvm $(whoami)
+sudo usermod -aG libvirt $(whoami)
+sudo usermod -aG input $(whoami)
+
+./fetch-macOS-v2.py -s ventura --action download
+dmg2img -i BaseSystem.dmg BaseSystem.img
+
+qemu-img create -f qcow2 mac_hdd_ng.img "$hdd_size"G
+
+cd ..
+
+echo "macOS ready to boot and install, please execute the run script!"
---
+++ startup-mac.sh
@@ -0,0 +1,5 @@
+sudo modprobe kvm; echo 1 | sudo tee /sys/module/kvm/parameters/ignore_msrs
+
+cd OSX-KVM-highspec
+chmod +x ./OpenCore-Boot.sh
+./OpenCore-Boot.sh
